4081196a1b8ea47320a97c3d2de7c3db9d1c96f7
bessemazizi <bessem.azizi@machinestalk.com>
2024-01-29 16:29:42 +0100

    Initial commit

---
+++ src/main/java/com/example/javase11developer1z0819ocp/JavaSe11Developer1Z0819OcpApplication.java
@@ -0,0 +1,13 @@
+package com.example.javase11developer1z0819ocp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSe11Developer1Z0819OcpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSe11Developer1Z0819OcpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javase11developer1z0819ocp/JavaSe11Developer1Z0819OcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javase11developer1z0819ocp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSe11Developer1Z0819OcpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
